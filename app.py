57e26cb7380fcf1c646056d186b44a705551df76
Nathan Kosiba <kosiba384@gmail.com>
2020-03-22 14:20:42 -0400

    add first flask endpoint

---
+++ app.py
@@ -0,0 +1,36 @@
+# import dependencies 
+import numpy as np
+import pandas as pd
+import json
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+from flask import Flask, jsonify
+
+# create database connection and create files to be loaded into API endpoints
+Base = automap_base()
+engine = create_engine('sqlite:///static/data/bredat.sqlite')
+Base.prepare(engine, reflect=True)
+Breweries = json.loads(pd.read_sql("SELECT * FROM breweries ORDER BY ID",con=engine).to_json(orient='records'))
+
+# create Flask app
+app = Flask(__name__)
+
+# create api endpoints
+@app.route("/")
+def main():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/breweries<br/>"
+    )
+
+@app.route("/api/v1.0/breweries")
+def apibreweries():
+    #create endpoint from breweries data
+    return jsonify(Breweries)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
