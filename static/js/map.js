cb60de153680afa1399f00ba682a20e8350cc238
Nathan Kosiba <kosiba384@gmail.com>
2020-03-22 21:07:53 -0400

    work on app

---
+++ static/js/map.js
@@ -0,0 +1,11 @@
+// Creating map object
+var myMap = L.map("map", {
+  center: [45.52, -122.67],
+  zoom: 13
+});
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
