fad90a481adef03fe33d02ee27263f992246e3a2
Nathan Kosiba <kosiba384@gmail.com>
2020-03-24 18:58:25 -0400

    update front-end app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-# create api endpoints
+# create front-end endpoints
 @app.route("/")
 def main():
     return '/brewwerymap'
@@ -15,7 +15,6 @@ def main():
 @app.route("/brewwerymap")
 def brewmap():
     return render_template("index.html")
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=4000)
