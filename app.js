12f8bee67c0d4f5955898770c9f83b40be5ecd67
Nathan Kosiba <kosiba384@gmail.com>
2020-03-28 08:51:58 -0400

    reorganize master files

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const port = 4000
 
 app.use('/static', express.static(__dirname + '/static'))
 app.get('/', (req, res) => res.sendFile(__dirname + "/templates/splash.html"))
-app.get('/brewerymap', (req, res) => res.sendFile(__dirname + "/templates/brewerymap.html"))
+app.get('/brewerymap', (req, res) => res.sendFile(__dirname + "/templates/imap.html"))
+app.get('/ogmap', (req, res) => res.sendFile(__dirname + "/templates/brewerymap.html"))
 app.get('/yearmap', (req, res) => res.sendFile(__dirname + "/templates/yearmap.html"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
