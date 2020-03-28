eca98fe502db465e338762d83eec7ac90881a3f3
Nathan Kosiba <kosiba384@gmail.com>
2020-03-28 08:55:41 -0400

    update app endpoints

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express()
 const port = 4000
 
 app.use('/static', express.static(__dirname + '/static'))
-app.get('/', (req, res) => res.sendFile(__dirname + "/templates/splash.html"))
+// main page
+app.get('/', (req, res) => res.sendFile(__dirname + "/templates/index.html"))
+//aps
 app.get('/brewerymap', (req, res) => res.sendFile(__dirname + "/templates/imap.html"))
-app.get('/ogmap', (req, res) => res.sendFile(__dirname + "/templates/brewerymap.html"))
 app.get('/yearmap', (req, res) => res.sendFile(__dirname + "/templates/yearmap.html"))
+// data table 
+app.get('/data', (req, res) => res.sendFile(__dirname + "/templates/dtable.html"))
+// brewery selection page
+app.get('/breweryselect', (req, res) => res.sendFile(__dirname + "/templates/selbrew.html"))
+// history page
+app.get('/history', (req, res) => res.sendFile(__dirname + "/templates/history.html"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
