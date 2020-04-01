1d1efea3274caaeb3b7ee87535dc1942a809538c
Nathan Kosiba <kosiba384@gmail.com>
2020-03-31 21:24:16 -0400

    update source data for map

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ Base = automap_base()
 engine = create_engine('sqlite:///static/data/bredat.sqlite')
 Base.prepare(engine, reflect=True)
 
-Breweries = json.loads(pd.read_sql("SELECT * FROM breweries ORDER BY NAME",con=engine).to_json(orient='records'))
+Breweries = json.loads(pd.read_sql("SELECT * FROM business ORDER BY id",con=engine).to_json(orient='records'))
 
 Localbusinesses = json.loads(pd.read_sql("SELECT * FROM localbusinesses ORDER BY id",con=engine).to_json(orient='records'))
 
